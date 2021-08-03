627fb9d6ee2897f20c9e04cfee9121937e6ddfe0
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-08-03 14:41:22 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.header__burger-menu').click(function(){
+        $('.header__burger-menu').toggleClass('header__menu-open');
+        $('.header__menu').toggleClass('header__menu-open');
+        $('.header__burder-img').toggleClass('header__menu-remove');
+        $('body').toggleClass('fixed-page');
+        $('header').toggleClass('skroll');
+    });
+});
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
